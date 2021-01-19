d54b82a7c532ece29dc0346f7c67253724e74320
Dan <dekay01@hotmail.com>
2021-01-19 10:30:44 -0500

    Added initial skelleton class and configured the .NET framework to be 4.8.

---
+++ smtpclient/EmailClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace smtpclient
+{
+    public class EmailClient
+    {
+        private string server = "";
+        private int port = 0;
+
+
+
+        public int Send(string fromAddress, string [] toAddresses, string [] ccAddresses, string [] bccAddresses, string subject, string body, string [] attachments, string [] attachmentNames)
+        {
+            int success = 1;
+            return success;
+        }
+
+        public void SetPort(int port)
+        {
+            this.port = port;
+        }
+
+        public void SetServer(string server)
+        {
+            this.server = server;
+        }
+    }
+}
